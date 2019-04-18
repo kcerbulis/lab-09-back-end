fdc26faac793f86962a62629e986ef6d491ac69c
evanpettie <evanpettie@gmail.com>
2019-04-18 12:23:33 -0700

    lab 8 done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 
 //global constants
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
@@ -34,36 +34,48 @@ app.use('*', (request, response) => {
 //sql commands 
 const SQL_CMDS = {};
 SQL_CMDS.getLocation = 'SELECT * FROM locations WHERE search_query=$1'
+// SQL_CMDS.getLocation = 'SELECT * FROM $1 WHERE search_query=$2'
 SQL_CMDS.insertLocation = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)'
 SQL_CMDS.getWeather = 'SELECT * FROM weathers WHERE location_id=$1'
 SQL_CMDS.insertWeather = 'INSERT INTO weathers (forecast, time, location_id) VALUES ($1, $2, $3)'
 
 //Constructor Functions
-function LocationData(search_query, formatted_query, latitude, longitude){
+function LocationData(search_query, formatted_query, latitude, longitude) {
   this.search_query = search_query;
   this.formatted_query = formatted_query;
   this.latitude = latitude;
   this.longitude = longitude;
 }
 
-function WeatherData(summary, time){
+function WeatherData(summary, time) {
   this.forecast = summary;
   this.time = time;
 }
 
 //Other Functions
-function searchLocationData(request, response) {
-
-  //user input - ex: if they type in Seattle...search_quer = Seattle
-  const search_query = request.query.data;
-  client.query(SQL_CMDS.getLocation, [search_query]).then(result => {
-    if(result.rows.length) {
+function checkDatabase(search_query, response) {
+//  return client.query(SQL_CMDS.getLocation, ['locations', search_query]).then(result => {
+  return client.query(SQL_CMDS.getLocation, [search_query]).then(result => {
+    if (result.rows.length) {
+      console.log("checking");
       response.send(result.rows[0])
     } else {
+      return 'NOT IN DATABASE';
+    }
+  });
+}
+
+function searchLocationData(request, response) {
+
+  const search_query = request.query.data;
+  checkDatabase(search_query, response).then(result => {
+    if (result === 'NOT IN DATABASE') {
+
       const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${search_query}&key=${process.env.GEOCODE_API_KEY}`;
 
       superagent.get(URL).then(result => {
-        if(result.body.status === 'ZERO_RESULTS'){
+        console.log("asking google");
+        if (result.body.status === 'ZERO_RESULTS') {
           response.status(500).send('Sorry, something went wrong');
           return;
         }
@@ -78,7 +90,7 @@ function searchLocationData(request, response) {
 
         //Create new object containing user input data
         //responseDataObject = {Seattle, Lynnwood, WA, USA, somenumber, somenumber}
-        
+
         response.send(responseDataObject);
       })
     }
@@ -90,13 +102,13 @@ function searchWeatherData(request, response) {
   const URL = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
   superagent.get(URL).then(result => {
 
-    if(result.body.latitude === Number(request.query.data.latitude) && result.body.longitude === Number(request.query.data.longitude)){
+    if (result.body.latitude === Number(request.query.data.latitude) && result.body.longitude === Number(request.query.data.longitude)) {
       //dailyData = array of daily data objects
       let dailyData = result.body.daily.data;
       const dailyWeather = dailyData.map((dailyDataObj) => {
         let summary = dailyDataObj.summary;
-        let time = new Date(dailyDataObj.time * 1000).toString().slice(0, 15) ;
-  
+        let time = new Date(dailyDataObj.time * 1000).toString().slice(0, 15);
+
         //For each entry within dailyData array
         //Create new weather object
         return new WeatherData(summary, time);
