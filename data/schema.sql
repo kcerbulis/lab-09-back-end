41d66ed1bcf346a5ca172ca5d0955e6d081d0b52
Jon-Ramer <ramerj@att.net>
2019-04-19 13:13:23 -0700

    movies & schema

--- data/schema.sql
+++ data/schema.sql
@@ -16,3 +16,13 @@ CREATE TABLE weathers (
   location_id INTEGER NOT NULL,
   FOREIGN KEY (location_id) REFERENCES locations (id)
 );
+
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  overview VARCHAR(255),
+  average_votes NUMERIC(3,2),
+  popularity NUMERIC(5,4),
+  released_on VARCHAR(255)
+  FOREIGN KEY (location_id) REFERENCES locations (id)
+)
